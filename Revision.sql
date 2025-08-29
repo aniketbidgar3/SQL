0a99143455779b9cf1613385f79b44677ee3f55c
aniketbidgar3 <aniketbidgar3@gmail.com>
2025-08-29 23:39:30 +0530

    "Started Learning SQL"

---
+++ Revision.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE Learning;
+
+-- SHOW DATABASES;
+
+-- USE Learning;
+
+CREATE TABLE Student(
+	roll INT ,
+    name VARCHAR(20)
+);
+
+SELECT * FROM Student;
+
+-- DROP DATABASE Learning;
+
